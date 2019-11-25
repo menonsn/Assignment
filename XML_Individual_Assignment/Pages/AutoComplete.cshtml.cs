7668008d2ed50019be31116bf3ed8e4ddd21ae8a
menonsn <54641774+menonsn@users.noreply.github.com>
2019-11-24 22:42:31 -0500

    Initial commit

---
+++ XML_Individual_Assignment/Pages/AutoComplete.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XML_Individual_Assignment.Pages
+{
+    public class AutoCompleteModel : PageModel
+    {
+        [BindProperty]
+        private string Term { get; set; }
+        private IList<string> Names = new List<string>();
+
+        public JsonResult OnGet()
+        {
+            Term = HttpContext.Request.Query["term"];
+
+            AddPlayers("Mumbai");
+            AddPlayers("Bangalore");
+            AddPlayers("Chennai");
+            AddPlayers("Hyderabad");
+            AddPlayers("Delhi");
+            return new JsonResult(Names);
+        }
+
+        private void AddPlayers(string Name)
+        {
+            if (Name.Contains(Term))
+            {
+                Names.Add(Name);
+            }
+
+        }
+
+    }
+}
---
+++ XML_Individual_Assignment/Pages/ConsumeXML.cshtml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XML_Individual_Assignment.Pages
+{
+    public class ConsumeXMLModel : PageModel
+    {
+        private IHostingEnvironment _environment;
+        private string result = "";
+        public ConsumeXMLModel(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            string fileName = Upload.FileName;
+            string file = Path.Combine(_environment.ContentRootPath, "uploads", fileName);
+            using (var fileStream = new FileStream(file, FileMode.Create))
+            {
+                Upload.CopyTo(fileStream);
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(file);
+            XmlNode node = doc.SelectSingleNode("/users/user");
+            ValidateXML(file);
+
+        }
+
+        private void ValidateXML(string file)
+        {
+            // delcare our validation preferences
+            XmlReaderSettings settings = new XmlReaderSettings();
+            string xsdPath = Path.Combine(_environment.ContentRootPath, "uploads", "uservalidation.xsd");
+            settings.Schemas.Add(null, xsdPath);
+
+            // validate with XSD
+            settings.ValidationType = ValidationType.Schema;
+
+            // a couple more flags.
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ProcessSchemaLocation;
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ReportValidationWarnings;
+
+            // who do we call when things go wrong?
+            settings.ValidationEventHandler += new System.Xml.Schema.ValidationEventHandler(this.ValidationEventHandler);
+
+            XmlReader xmlReader = XmlReader.Create(file, settings);
+            try
+            {
+                while (xmlReader.Read())
+                {
+
+                }
+                ViewData["result"] = "Validated!";
+            }
+            catch (Exception e)
+            {
+                // we only get here if there is a validation error.
+                ViewData["result"] = e.Message;
+            }
+
+
+        }
+
+        private void ValidationEventHandler(object sender, ValidationEventArgs args)
+        {
+            result = "validation failed.  Message: " + args.Message;
+            throw new Exception("validation failed.  Message: " + args.Message);
+        }
+    }
+}
